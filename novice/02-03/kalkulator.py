7463e4cbcc00ad95315e9f17c69c09db78c6fdbf
muhevri <muhevri@gmail.com>
2020-12-16 16:41:23 +0700

    hari ketiga pekan ke 2

---
+++ novice/02-03/kalkulator.py
@@ -0,0 +1,18 @@
+class Kalkulator:
+
+# METHODS
+ def jumlahkan(self, a, b):
+    return a + b
+
+ def kurangkan(self, a, b):
+    return a - b
+
+ def kalikan(self, a, b):
+    return a * b
+
+ def bagikan(self, a, b):
+    return a / b
+
+casio = Kalkulator()
+hasil = casio.jumlahkan(2, 3)
+print(hasil)
